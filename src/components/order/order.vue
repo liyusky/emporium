9bb7acc8186b295140436e60fa27e9edf8f27e5f
CzTwo <17730127131@163.com>
2018-07-06 18:09:00 +0800

    modify order v 0.2

--- src/components/order/order.vue
+++ src/components/order/order.vue
@@ -2,38 +2,51 @@
   <section class="order">
     <Theme :theme="theme"></Theme>
     <section class="order-tabbar">
-      <div class="tabbar-item">全部</div>
-      <div class="tabbar-item">代付款</div>
-      <div class="tabbar-item">代发货</div>
-      <div class="tabbar-item">代收货</div>
-      <div class="tabbar-item">待评价</div>
+      <div class="tabbar-item" :class="{active:checkPageName == 'orderAll'}" @click="checkPage('orderAll')">全部</div>
+      <div class="tabbar-item" :class="{active:checkPageName == 'orderPay'}" @click="checkPage('orderPay')">代付款</div>
+      <div class="tabbar-item" :class="{active:checkPageName == 'orderSend'}" @click="checkPage('orderSend')">代发货</div>
+      <div class="tabbar-item" :class="{active:checkPageName == 'orderReceipt'}" @click="checkPage('orderReceipt')">代收货</div>
+      <div class="tabbar-item" :class="{active:checkPageName == 'orderEvaluate'}" @click="checkPage('orderEvaluate')">待评价</div>
     </section>
     <section class="order-classify">
-      <router-view></router-view>
+      <OrderList :orderList = orderList></OrderList>
     </section>
   </section>
 </template>
 
 <script>
+import Http from '../../class/http.class.js'
 import Theme from '../common/theme/theme.vue'
+import OrderList from './order-list/order-list.vue'
 export default {
   name: 'Order',
   data () {
     return {
       theme: {
-        title: 'iPhone 6S'
-      }
+        title: '我的订单'
+      },
+      checkPageName: 'orderAll',
+      orderList: null
     }
   },
   mounted () {
+    Http.request('http://localhost:3004/order', (data) => {
+      this.orderList = data
+    })
   },
   methods: {
+    checkPage (orderListName) {
+      this.checkPageName = orderListName
+    }
   },
   components: {
-    Theme
+    Theme,
+    OrderList
   }
 }
 </script>
+
 <style scoped lang="scss">
-@import './order.scss';
+  @import './order.scss';
+
 </style>
