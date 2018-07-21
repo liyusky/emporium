c6ed2513687d0de6c3ac70e66b2a9d52c1001a55
CzTwo <17730127131@163.com>
2018-07-21 17:02:05 +0800

    modify UI style

--- src/class/http.class.js
+++ src/class/http.class.js
@@ -5,8 +5,8 @@ export default class Http {
     axios({
       url: Url[args.url],
       method: 'post',
-      baseURL: 'http://192.168.0.101:8082',
-      // baseURL: 'http://api2.jietiaodashi.com',
+      // baseURL: 'http://192.168.0.101:8082',
+      baseURL: 'http://api2.jietiaodashi.com',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
