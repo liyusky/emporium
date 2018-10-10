6d5cdd31a58d0833790436614200dc57ffc0c3bf
“CZHub <1979250385@qq.com>
2018-10-10 18:17:05 +0800

    refresh

---
+++ src/components/home/shop/goods/goods.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- s  -->
+  <ul class="goods">
+    <div class="swiper-container goods-swiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" v-for="(item, index) in goods" :key="index">
+          <li class="goods-item">
+            <img class="item-image" :src="item.Icon">
+            <p class="item-title">{{item.Title}}</p>
+            <p class="item-prise">¥{{item.nowPrice}}</p>
+            <p class="item-staging">
+              <span>¥{{item.money}}</span>
+              <span>x{{item.time}}期</span>
+            </p>
+          </li>
+        </div>
+      </div>
+    </div>
+  </ul>
+  <!-- e  -->
+</template>
+
+<script>
+import Swiper from 'Swiper'
+export default {
+  name: 'Goods',
+  props: ['goods'],
+  data () {
+    return {
+      goodsSwiper: null
+    }
+  },
+  created () {
+    this.$nextTick(() => {
+      this.initGoodsSwiper()
+    })
+  },
+  methods: {
+    initGoodsSwiper () {
+      if (this.goodsSwiper !== null) return
+      this.goodsSwiper = new Swiper('.goods-swiper', {
+        slidesPerView: 3,
+        slidesPerGroup: 1,
+        spaceBetween: 10,
+        freeMode: true,
+        observer: true,
+        observeParents: true
+      })
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import "./goods.scss";
+</style>
